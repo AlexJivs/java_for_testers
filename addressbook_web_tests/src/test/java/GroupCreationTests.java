1670f152a0a2e7592963f178d465a1a8dc3546cd
AlexJivs <zhikh5@gmail.com>
2025-02-10 19:43:57 +0300

    модельные классы 3.2

--- addressbook_web_tests/src/test/java/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/GroupCreationTests.java
@@ -1,3 +1,4 @@
+import model.Groupdata;
 import org.junit.jupiter.api.Test;
 
 public class GroupCreationTests extends TestBase{
@@ -6,13 +7,36 @@ public class GroupCreationTests extends TestBase{
     @Test
     public void canCreateGroup() {
         openGroupsPage();
-
-        createGroup("group name", "group header", "group footer");
+        createGroup(new Groupdata("group name", "group header", "group footer"));
     }
 
     @Test
     public void canCreateGroupWithEmptyName() {
         openGroupsPage();
-        createGroup("", "", "");
+        createGroup(new Groupdata());
+    }
+
+    @Test
+    public void canCreateGroupWithNameOnly() {
+        openGroupsPage();
+        var emptyGroup = new Groupdata();
+        var groupWithName = emptyGroup.withName("some name");
+        createGroup(groupWithName);
+    }
+
+    @Test
+    public void canCreateGroupWithHeaderOnly() {
+        openGroupsPage();
+        var emptyGroup = new Groupdata();
+        var groupWithHeader = emptyGroup.withHeader("some header");
+        createGroup(groupWithHeader);
+    }
+
+    @Test
+    public void canCreateGroupWithFooterOnly() {
+        openGroupsPage();
+        var emptyGroup = new Groupdata();
+        var groupWithFooter = emptyGroup.withFooter("some footer");
+        createGroup(groupWithFooter);
     }
 }
--- addressbook_web_tests/src/test/java/model/Groupdata.java
+++ addressbook_web_tests/src/test/java/model/Groupdata.java
@@ -1,4 +1,21 @@
 package model;
 
-public record Groupdata(String group_name, String group_header, String group_footer) {
+public record Groupdata(String name, String header, String footer) {
+
+    public  Groupdata() {
+        this ("","","");
+    }
+
+    public Groupdata withName(String name) {
+        return new Groupdata(name, this.header, this.footer);
+    }
+
+    public Groupdata withHeader(String header) {
+        return new Groupdata(this.name, header, this.footer);
+    }
+
+    public Groupdata withFooter(String footer) {
+        return new Groupdata(this.name, this.header, footer);
+    }
+
 }
